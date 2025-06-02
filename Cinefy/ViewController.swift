b3896baecc9225978146b048d3270fc7fd9a0653
Yasin <osmanyasinkiziltas@aof.anadolu.edu.tr>
2025-06-02 22:10:14 +0300

    Initial Commit

---
+++ Cinefy/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Cinefy
+//
+//  Created by Yasin on 2.06.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
