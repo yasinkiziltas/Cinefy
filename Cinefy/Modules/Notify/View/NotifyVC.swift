788b52c8858551d35c60e789f87db1b53b3478c2
Yasin Kızıltaş <>
2025-08-05 23:07:51 +0300

    UI

---
+++ Cinefy/Modules/Notify/View/NotifyVC.swift
@@ -0,0 +1,15 @@
+//
+//  NotifyVC.swift
+//  Cinefy
+//
+//  Created by Yasin Kızıltaş on 5.08.2025.
+//
+
+import UIKit
+
+class NotifyVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
